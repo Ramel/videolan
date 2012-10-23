dca674189a5754bce73d120cc8839603ad0a3ee0
Armel FORTUN <armel@tchack.com>
2012-10-23 16:13:08 +0200

    Contribs

---
+++ contrib/i586-mingw32msvc/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/fortuna/Dev/projets/vlc-2.0/contrib/i586-mingw32msvc/lib"
+XML2_LIBS="-lxml2     "
+XML2_INCLUDEDIR="-I/home/fortuna/Dev/projets/vlc-2.0/contrib/i586-mingw32msvc/include/libxml2"
+MODULE_VERSION="xml2-2.8.0"
+
